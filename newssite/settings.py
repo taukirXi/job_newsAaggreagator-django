6ed9a74256b52b97070e4f506241a0413e565d84
taukir ahmmed <taukirahmed779@gmail.com>
2021-02-15 22:04:35 +0600

    fix

--- newssite/settings.py
+++ newssite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '9c5rjy0(agx194v77ofdbm9gxjuh_1=n9r$@-zo2hm^_j@@kz6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['falgunnewsaggregator.heroku.com']
 
 
 # Application definition
