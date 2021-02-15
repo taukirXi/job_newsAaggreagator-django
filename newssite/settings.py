04667bf0077a27eef444591653d165927965be8d
taukir ahmmed <taukirahmed779@gmail.com>
2021-02-15 21:18:33 +0600

    tweaked dubug

--- newssite/settings.py
+++ newssite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9c5rjy0(agx194v77ofdbm9gxjuh_1=n9r$@-zo2hm^_j@@kz6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
